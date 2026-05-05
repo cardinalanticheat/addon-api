b5b4861025384cd8d83e34aee8311377d8d42489
micartey <me@micartey.dev>
2026-05-05 21:38:59 +0200

    chore: some more cleanup

--- src/main/java/me/clientastisch/cardinal/extension/impl/command/Command.java
+++ src/main/java/me/clientastisch/cardinal/extension/impl/command/Command.java
@@ -9,7 +9,14 @@ import org.bukkit.command.CommandSender;
 public interface Command {
 
     /**
+     * Implement a command hook that will be executed by cardinal for custom commands.
+     * Here an example for the parameters when running the command "cac flag"
      *
+     * @param sender the command sender (player or console)
+     * @param command the command e.g. "cac"
+     * @param args array of arguments e.g. ["cac", "flag"]
+     * @param raw the raw string e.g. "/cac flag"
+     * @return true when the command matches
      */
     boolean execute(CommandSender sender, String command, String[] args, String raw);
 
